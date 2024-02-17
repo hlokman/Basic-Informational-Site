ac45981ce8d8cf5fdc9137d1b5e02bda76795402
hlokman <abdallahlokmanhenni@gmail.com>
2024-02-17 03:52:15 +0100

    Edit

--- index.js
+++ index.js
@@ -5,20 +5,19 @@ const { URL } = require("url");
 const server = http.createServer(async (req, res) => {
   //const myURL = new URL(`${req.url}`);
   let filePath = req.url === "/" ? "./index.html" : `.${req.url}.html`;
+  let data;
 
   try {
     res.writeHead(200, { "Content-Type": "text/html" });
-    const data = await fs.readFile(filePath);
-    res.write(data);
-    res.end(/*console.log("test")*/);
+    data = await fs.readFile(filePath);
   } catch (err) {
     console.log(err);
     res.writeHead(404, { "Content-Type": "text/html" });
-    const data = await fs.readFile("./404.html");
-    res.write(data);
-    res.end();
+    data = await fs.readFile("./404.html");
   }
 
+  res.write(data);
+  res.end();
   console.log(req.url);
 });
 
