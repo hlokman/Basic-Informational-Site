5d823f8329b61aa6f0252dfc0ccbd3367eeb6c49
hlokman <abdallahlokmanhenni@gmail.com>
2024-03-03 19:37:51 +0100

    Edit to a full express version

--- app.js
+++ app.js
@@ -3,7 +3,47 @@ const express = require("express");
 const app = express();
 const port = 8080;
 
-app.get("/", async (req, res) => {
+app.get("/", (req, res) => {
+  try {
+    res.sendFile(__dirname + "/index.html");
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Something broke!");
+  }
+});
+
+app.get("/about", (req, res) => {
+  try {
+    res.sendFile(__dirname + "/about.html");
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Something broke!");
+  }
+});
+
+app.get("/contact-me", (req, res) => {
+  try {
+    res.sendFile(__dirname + "/contact-me.html");
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Something broke!");
+  }
+});
+
+app.use((req, res) => {
+  try {
+    res.sendFile(__dirname + "/404.html");
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Something broke!");
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
+/*app.get("/", async (req, res) => {
   try {
     const data = await fs.readFile("./index.html", "utf-8");
     res.writeHead(200, { "Content-Type": "text/html" });
@@ -45,45 +85,4 @@ app.use(async (req, res) => {
     console.log(err);
     res.status(500).send("Something broke!");
   }
-});
-
-app.listen(port, () => {
-  console.log("Server running on ...");
-});
-
-/*app.get("/", (req, res) => {
-  try {
-    res.sendFile(__dirname + "/index.html");
-  } catch (err) {
-    console.log(err);
-    res.status(500).send("Something broke!");
-  }
-});
-
-app.get("/about", (req, res) => {
-  try {
-    res.sendFile(__dirname + "/about.html");
-  } catch (err) {
-    console.log(err);
-    res.status(500).send("Something broke!");
-  }
-});
-
-app.get("/contact-me", (req, res) => {
-  try {
-    res.sendFile(__dirname + "/contact-me.html");
-  } catch (err) {
-    console.log(err);
-    res.status(500).send("Something broke!");
-  }
-});
-
-app.use((req, res) => {
-  try {
-    res.sendFile(__dirname + "/404.html");
-  } catch (err) {
-    console.log(err);
-    res.status(500).send("Something broke!");
-  }
-});
-*/
+});*/
